dd3af5cb790769b1a5b50d39dc410f48492edcd3
Adel <htwr86@gmail.com>
2019-07-19 00:10:23 +0300

    123.5 index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './components/app/app';
+import ErrorBoundry from './components/error-boundry';
+import BookstoreService from './services/bookstore-service'
+import withBookstoreService from './components/hoc/with-bookstore-service';
+import store from './store';
 import 'tachyons';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
