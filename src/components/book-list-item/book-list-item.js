a707c906ef9521c70b2d7324677dc7c73cdf4e48
Adel <htwr86@gmail.com>
2019-07-19 19:50:37 +0300

    124.99

--- src/components/book-list-item/book-list-item.js
+++ src/components/book-list-item/book-list-item.js
@@ -3,12 +3,12 @@ import React, {Fragment} from 'react';
 import './book-list-item.css';
 
 const BookListItem = ({ book }) => {
+  console.log(book);
   const { title, author } = book;
   return (
     <Fragment>
-      <h3>Book</h3>
-      <span>author: {author}</span>
-      <span>title: {title}</span>
+      <span>author: {author} </span>
+      <span>title: {title} </span>
     </Fragment>
     
   );
