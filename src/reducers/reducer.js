6c1c64a82e9f0a4411b6e7d444a3f54e40891d06
Adel <htwr86@gmail.com>
2019-07-25 23:03:01 +0300

    136.99

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -6,6 +6,37 @@ const initialState = {
   orderTotal: 350
 };
 
+const updateCartItems = (cartItems, item, idx) => {
+
+  if (idx === -1) {
+    return [
+      ...cartItems,
+      item
+    ]
+  }
+  return [
+    ...cartItems.slice(0, idx),
+    item,
+    ...cartItems.slice(idx + 1)
+  ]
+};
+
+const updateCartItem = (book, item = {}) => {
+  const { 
+    id = book.id,
+    count = 0,
+    title = book.title,
+    total = 0 } = item;
+
+  return {
+    id, 
+    title, 
+    count: count + 1, 
+    total: total + book.price
+  };
+}
+
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_BOOKS_REQUEST':
@@ -36,41 +67,12 @@ const reducer = (state = initialState, action) => {
       const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
       const item = state.cartItems[itemIndex];
 
-      let newItem;
+      const newItem = updateCartItem(book, item);
 
-      if (item) {
-        newItem = {
-          ...item,
-          count: item.count + 1,
-          total: item.total + book.price
-        };
-      } else {
-        newItem = {
-          id: book.id,
-          title: book.title,
-          count: 1,
-          total: book.price
-        };
-      }
-
-      if(itemIndex < 0) {
-        return {
-          ...state,
-          cartItems: [
-            ...state.cartItems,
-            newItem
-          ]
-        };
-      } else {
-        return {
-          ...state,
-          cartItems: [
-            ...state.cartItems.slice(0, itemIndex),
-            newItem,
-            ...state.cartItems.slice(itemIndex + 1)
-          ]
-        }; 
-      }
+      return { 
+        ...state,
+        cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
+      }; 
       
     default :
       return state;
