07989bc1d6a4a5847bd28ca06dd96d25f01400f7
Adel <htwr86@gmail.com>
2019-07-25 15:46:02 +0300

    135.4 adding an element to array

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,13 +10,14 @@ import ErrorIndicator from '../error-indicator';
 
 import './book-list.css';
 
-const BookList = ({ books }) => {
+const BookList = ({ books, onAddedToCart }) => {
   return (
     <ul className='book-list'>
       {
         books.map((book) => {
           return (
-            <li key={book.id}><BookListItem  book={book} /></li>
+            <li key={book.id}>
+              <BookListItem  book={book} onAddedToCart={()=>onAddedToCart(book.id)}/></li>
           )
         })
       }
@@ -32,7 +33,7 @@ class BookListContainer extends Component {
 
   render() {
 
-    const { books, loading, error } = this.props;
+    const { books, loading, error, onAddedToCart } = this.props;
     if (loading) {
       return <Spinner />
     }
@@ -40,7 +41,7 @@ class BookListContainer extends Component {
     if (error) {
       return <ErrorIndicator errorMessage={error}/>
     }
-    return <BookList books={books}/>;
+    return <BookList books={books} onAddedToCart={onAddedToCart}/>;
   };
 }
 
@@ -50,7 +51,8 @@ const mapStateToProps = ({ books, loading, error }) => {
 
 const mapDispatchToProps = (dispatch, { bookstoreService }) => {
   return {
-    fetchBooks: fetchBooks(bookstoreService, dispatch)
+    fetchBooks: fetchBooks(bookstoreService, dispatch),
+    onAddedToCart: (id)=>console.log(id,'action')
   };
 };
 
