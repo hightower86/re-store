48fcb6cf9a50f4af6aabb52f230cda932679d512
Adel <htwr86@gmail.com>
2019-07-27 20:48:37 +0300

    139.99 store engancers

--- src/store.js
+++ src/store.js
@@ -1,8 +1,35 @@
-import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
+import { createStore, compose } from 'redux';
+// import logger from 'redux-logger';
 
 import reducer from './reducers';
 
-const store = createStore(reducer, applyMiddleware(logger));
+const logEnhancer = (createStore) => (...args) => {
+  const store = createStore(...args);
+  const originalDispatch = store.dispatch;
+  store.dispatch = (action) => {
 
+    console.log(action.type);
+    return originalDispatch(action);
+  };
+  return store;
+};
+
+const stringEnhancer = (createStore) => (...args) => {
+  const store = createStore(...args);
+  const originalDispatch = store.dispatch;
+  store.dispatch = (action) => {
+
+    if (typeof action === 'string') {
+      return originalDispatch({
+        type: action
+      });
+    }
+    return originalDispatch(action);
+  };
+  return store;
+};
+
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+
+store.dispatch({type: 'STRING_RANDOM'});
 export default store;
